273061fc86dca9ab43fc87c596c230002d3fb370
Daniel Feria <daniferia3105gmail.com>
2025-11-12 00:15:28 -0500

    tercera subida

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(helmet());
 
 // Configuración de CORS
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'https://maxwavex-web.vercel.app',
     credentials: true
 }));
 
